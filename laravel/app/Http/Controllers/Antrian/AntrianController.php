1a1b951f8f768e58bfe07bac7dd20ec490b0ed0b
Indra Mista <indra.mista@mst.co.id>
2023-03-24 16:59:41 +0700

    feat(antrian): add page antrian

--- laravel/app/Http/Controllers/Antrian/AntrianController.php
+++ laravel/app/Http/Controllers/Antrian/AntrianController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Antrian;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AntrianModel;
 
 class AntrianController extends Controller
 {
@@ -11,5 +12,8 @@ class AntrianController extends Controller
     public function index()
     {
         return view('antrian.index');
+    }
+    public function getAntrian(){
+
     }
 }
