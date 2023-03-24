2ff615d935d2116a19da43712dd12605a61cdef1
Indra Mista <indra.mista@mst.co.id>
2023-03-24 10:05:14 +0700

    chore: add template

---
+++ laravel/resources/views/templates.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from preclinic.dreamguystech.com/template/register.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 Mar 2023 01:19:06 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('/assets')}}/img/favicon.png">
+    <title>Preclinic - Medical & Hospital - Bootstrap 5 Admin Template</title>
+
+    <link rel="stylesheet" type="text/css" href="{{url('/assets')}}/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="{{url('/assets')}}/plugins/feather/feather.css">
+
+    <link rel="stylesheet" href="{{url('/assets')}}/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="{{url('/assets')}}/plugins/fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" type="text/css" href="{{url('/assets')}}/css/style.css">
+</head>
+
+<body>
+
+    <div class="main-wrapper login-body">
+        <div class="container-fluid px-0">
+            <div class="row">
+
+                <div class="col-lg-6 login-wrap">
+                    <div class="login-sec">
+                        <div class="log-img">
+                            <img class="img-fluid" src="{{url('/assets')}}/img/login-02.png" alt="Logo">
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-6 login-wrap-bg">
+                    <div class="login-wrapper">
+                        <div class="loginbox">
+                            <div class="login-right">
+                                <div class="login-right-wrap">
+                                    <div class="account-logo">
+                                        <a href="index.html"><img src="{{url('/assets')}}/img/login-logo.png" alt=""></a>
+                                    </div>
+                                    <h2>Getting Started</h2>
+
+                                    <form action="https://preclinic.dreamguystech.com/template/login.html">
+                                        <div class="form-group">
+                                            <label>Full Name <span class="login-danger">*</span></label>
+                                            <input class="form-control" type="text">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Email <span class="login-danger">*</span></label>
+                                            <input class="form-control" type="text">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Password <span class="login-danger">*</span></label>
+                                            <input class="form-control pass-input" type="password">
+                                            <span class="profile-views feather-eye-off toggle-password"></span>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Confirm Password <span class="login-danger">*</span></label>
+                                            <input class="form-control pass-input-confirm" type="password">
+                                            <span class="profile-views feather-eye-off confirm-password"></span>
+                                        </div>
+                                        <div class="form-group login-btn">
+                                            <button class="btn btn-primary btn-block" type="submit">Sign up</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <script src="{{url('/assets')}}/js/jquery-3.6.1.min.js"></script>
+
+    <script src="{{url('/assets')}}/js/bootstrap.bundle.min.js"></script>
+
+    <script src="{{url('/assets')}}/js/feather.min.js"></script>
+
+    <script src="{{url('/assets')}}/js/app.js"></script>
+</body>
+
+<!-- Mirrored from preclinic.dreamguystech.com/template/register.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 Mar 2023 01:19:06 GMT -->
+
+</html>
