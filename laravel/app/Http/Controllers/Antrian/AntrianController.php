b2b69c139c67934583c2c5caa53618683d88d7a0
Indra Mista <indra.mista@mst.co.id>
2023-03-25 21:04:39 +0700

    feat(antrian): get antrian from api

--- laravel/app/Http/Controllers/Antrian/AntrianController.php
+++ laravel/app/Http/Controllers/Antrian/AntrianController.php
@@ -5,19 +5,42 @@ namespace App\Http\Controllers\Antrian;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\AntrianModel;
+use App\Models\CounterModel;
 
 class AntrianController extends Controller
 {
 
     public function index()
     {
-        return view('antrian.index');
+        $counter=CounterModel::where('no_counter','<>','1')->get();
+        return view('antrian.index',compact('counter'));
     }
     public function print(){
         return view('antrian.print');
     }
 
     public function getAntrian(){
-
+        $data = AntrianModel::where('status',0)
+        ->where('id_counter','<>',0)->first();
+        $message="";
+        if($data){
+            $pre = $data->type_antrian==1?'A':'U';
+            $code ="";
+            if($data->no_antrian<10){
+                $code=" 0 0 ".(int)$data->no_antrian;
+            }else if($data->no_antrian>=10 && $data->no_antrian<100){
+                $code=" 0 ".(int)$data->no_antrian;
+            }
+            AntrianModel::where('id', $data->id)
+            ->update(['status' => 1,'updated_at'=>null]);
+            $message="Nomor Antrian ".$pre.$code." di Loket ".$data->id_counter;
+        }
+        $antrian = AntrianModel::whereNull('updated_at')
+            ->where('id_counter','<>',0)->get();
+        return response()->Json([
+            'status'=>'success',
+            'antrian'=>$antrian,
+            'message'=> $message
+        ]);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Antrian\AntrianController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::controller(AntrianController::class)->group(function () {
+    Route::get('/antrian', 'getAntrian');
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
